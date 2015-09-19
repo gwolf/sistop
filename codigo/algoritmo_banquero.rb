3601d7e5dd125f8bbb9e1f38beb3e9f7e6297085
Gunnar Wolf <gwolf@gwolf.org>
2015-09-19 01:06:44 -0500

    Simple pero importante corrección en el algoritmo del banquero

--- codigo/algoritmo_banquero.rb
+++ codigo/algoritmo_banquero.rb
@@ -1,10 +1,15 @@
 l = ['A', 'B', 'C', 'D', 'E']; # Todos los procesos del sistema
 s = []; # Secuencia segura
+
+reclamado = {'A' => 4, 'B' => 2, 'C' => 2, 'D' => 1, 'E' => 5}
+asignado = {'A' => 1, 'B' => 1, 'C' => 2, 'D' => 0, 'E' => 3}
+libres = 2
+
 while ! l.empty? do
-  p = l.select {|id| reclamado[id] - asignado[id] > libres}.first
+  p = l.select {|id| reclamado[id] - asignado[id] <= libres}.first
   raise Exception, 'Estado inseguro' if p.nil?
   libres += asignado[p]
   l.delete(p)
   s.push(p)
 end
-puts "La secuencia segura encontrada es: %s" % s
+puts "La secuencia segura encontrada es: " + s.to_s

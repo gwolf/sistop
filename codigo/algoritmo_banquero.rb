0582aa5e35926b7aa60fc3ab5044196c7de2e53d
Emilio Cobos Álvarez <ecoal95@gmail.com>
2016-01-01 23:12:36 +0100

    codigo: Usar el método `.find` en vez de `.select {}.first`
    Hace lo mismo, pero es algo más idiomático y rápido.

--- codigo/algoritmo_banquero.rb
+++ codigo/algoritmo_banquero.rb
@@ -6,10 +6,11 @@ asignado = {'A' => 1, 'B' => 1, 'C' => 2, 'D' => 0, 'E' => 3}
 libres = 2
 
 while ! l.empty? do
-  p = l.select {|id| reclamado[id] - asignado[id] <= libres}.first
+  p = l.find {|id| reclamado[id] - asignado[id] <= libres}
   raise Exception, 'Estado inseguro' if p.nil?
   libres += asignado[p]
   l.delete(p)
   s.push(p)
 end
+
 puts "La secuencia segura encontrada es: " + s.to_s
